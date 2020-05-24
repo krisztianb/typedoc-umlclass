2a1a28b40e2639e32775dbb999fbfb378bd989e0
Krisztian Balla <k.balla@gmx.at>
2020-05-24 12:13:36 +0200

    Fix data urls can't be clicked in browsers

--- src/typedoc/page_section.ts
+++ src/typedoc/page_section.ts
@@ -14,11 +14,22 @@ export class PageSection {
      * @returns The HTML for the section.
      */
     public static createForHierarchyDiagram(title: string, imageUrl: string, reflectionName: string): string {
-        return `<section class="tsd-panel tsd-hierarchy-diagram">
-                    <h3>${title}</h3>
-                    <a class="uml-class" href="${imageUrl}" title="Click to enlarge">
-                        <img src="${imageUrl}" alt="UML class diagram of ${reflectionName}" />
-                    </a>
-                </section>\n`;
+        // URLs with data are not clickable in browsers for security concerns
+        if (imageUrl.startsWith("data:")) {
+            return `<section class="tsd-panel tsd-hierarchy-diagram">
+                        <h3>${title}</h3>
+                        <img class="uml-class" src="${imageUrl}"
+                             alt="UML class diagram of ${reflectionName}"
+                             title="Right click and select 'View image' to enlarge" />
+                    </section>\n`;
+        } else {
+            return `<section class="tsd-panel tsd-hierarchy-diagram">
+                        <h3>${title}</h3>
+                        <a class="uml-class" href="${imageUrl}" title="Click to enlarge">
+                            <img src="${imageUrl}"
+                                 alt="UML class diagram of ${reflectionName}" />
+                        </a>
+                    </section>\n`;
+        }
     }
 }
